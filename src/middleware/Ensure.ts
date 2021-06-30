9788322cd8eeb185d8eaeb4dd768c071ffd177a7
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-06-30 11:23:58 -0300

    auth bug

--- src/middleware/Ensure.ts
+++ src/middleware/Ensure.ts
@@ -19,20 +19,26 @@ export function ensureAuthenticate(req: Request, res: Response, next: NextFuncti
    }
 
    //Removing the 'Bearer'
-   const [,token] = authToken.split('')
+   const [ , token] = authToken.split(' ');
+   console.log( token);
 
    //Verify if the token is valid 
 
    try{
 
-    const { sub } = verify(token, "243hjjSKdhlkkljdk4844r3erefdfdf") as Payload ;
+    console.log("Cheguei no try");
+
+    const { sub } = verify(token, "243hjjSKdhlkkljdk4844r3erefdfdf") as Payload;
+
+    console.log('Passei no sub');
 
     req.user_id = sub;
 
   
     }  catch(e) {
 
-    return res.status(401).end();
+    console.log('Caí no catch');
+    return res.status(401).json({Erro: "Error"});
 
    }
 
